81dd443a2010beca597196bbd21882461eefd1b5
Anees Mohideen <aneesmohid@gmail.com>
2022-05-26 11:45:54 -0500

    asdf

--- app.py
+++ app.py
@@ -41,11 +41,16 @@ def favcities():
     temp_data = {}
     for c in citylist:
         temp_get = index2(c)['temp']
-        print("temp_get: ")
-        print(temp_get)
         temp_data[c] = temp_get
     print(temp_data)
-    response1 = jsonify(temp_data)
+    favList = []
+    for city in temp_data:
+        temp = temp_data[city]
+        print(city, temp)
+        obj = {"city": city, "temp": temp}
+        favList.append(obj)
+
+    response1 = jsonify(favList)
 
     return response1
 
@@ -79,23 +84,25 @@ def index(name):
 
 def index2(name):
     fail_data = {'temp': "N/A", 'forecast': 'N/A'}
-
-    url = "https://open.mapquestapi.com/geocoding/v1/address?key=A9AWLA8Gm8L5c5KtZ5IT82dhGlO2iZAw&location=" + name
+    name1 = name.split();
+    name_url = '+'.join(name1)
+    url = "https://open.mapquestapi.com/geocoding/v1/address?key=A9AWLA8Gm8L5c5KtZ5IT82dhGlO2iZAw&location=" + name_url
+    print(url)
     try:
         cds1 = requests.get(url)
     except Exception as e:
-        return jsonify(fail_data), 400
-    if cds1.json()["results"][0]["locations"][0]["adminArea5"] == "":
-        return jsonify(fail_data), 400
+        return fail_data
+
     lat1 = cds1.json()["results"][0]["locations"][0]["latLng"]["lat"]
     long1 = cds1.json()["results"][0]["locations"][0]["latLng"]["lng"]
 
+    # Check if data is the default fail output lat/lng
+    if lat1 == 39.78373 and long1 == -100.445882:
+        return fail_data
+
     gridpts = requests.get("https://api.weather.gov/points/" + str(lat1) + "," + str(long1))
-    print(gridpts.json()["properties"]["forecast"])
     gp_response1 = gridpts.json()["properties"]["forecast"]
     response1 = requests.get(gp_response1)
-    print_temp = str(response1.json()['properties']['periods'][0]['temperature'])
-    print("TEMPERATURE: " + print_temp)
 
     temp1 = response1.json()['properties']['periods'][0]['temperature']
     s_temp1 = str(temp1)
